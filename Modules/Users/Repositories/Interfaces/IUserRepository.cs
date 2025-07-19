d2da022b37c8fcc2d1224f4fcb248a7f9ddbdf0d
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2025-07-19 11:47:15 +0700

    feat: add module users

--- Modules/Users/Repositories/Interfaces/IUserRepository.cs
+++ Modules/Users/Repositories/Interfaces/IUserRepository.cs
@@ -2,14 +2,14 @@ namespace SchoolManagementSystem.Users.Repositories.Interfaces;
 
 using SchoolManagementSystem.Entities;
 
-    public interface IUserRepository
-    {
-        Task<User> AddUserAsync(User user);
-        Task<int> CountStudentsCreatedTodayAsync();
-        Task<User?> FindByFullNameAsync(string fullName);
+public interface IUserRepository
+{
+    Task<User> AddUserAsync(User user);
+    Task<int> CountStudentsCreatedTodayAsync();
+    Task<User?> FindByFullNameAsync(string fullName);
 
-        Task<User?> FindByIdAsync(int id);
+    Task<User?> FindByIdAsync(int id);
 
-        Task UpdateUserAsync(User user);
-        Task DeleteUserAsync(int userId);
-    }
+    Task UpdateUserAsync(User user);
+    Task DeleteUserAsync(int userId);
+}
